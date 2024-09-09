4b796520d95a487021e03661ceb8e012af3e3d15
kar1221 <khyeong191@gmail.com>
2024-09-09 22:21:56 +0800

    remove unnecessary rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,15 +5,5 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 export default withNuxt(
   // Your custom configs here
 
-  eslintPrettier,
-  {
-    rules: {
-      'vue/multi-word-component-names': [
-        'error',
-        {
-          ignores: ['default', 'index']
-        }
-      ]
-    }
-  }
+  eslintPrettier
 );
