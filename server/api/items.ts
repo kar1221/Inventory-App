17286216f7f6811a12fba4b0c1cdef296093d3fe
kar1221 <khyeong191@gmail.com>
2024-09-11 16:46:21 +0800

    normalize prisma's unnecessary nesting

--- server/api/items.ts
+++ server/api/items.ts
@@ -1,9 +1,10 @@
 import prisma from '~/lib/prisma';
 
 export default defineEventHandler(async () => {
-  const data = await prisma.items.findMany({
+  const items = await prisma.items.findMany({
     select: {
       item_name: true,
+      id: true,
       categories: {
         select: {
           category: {
@@ -16,5 +17,37 @@ export default defineEventHandler(async () => {
     }
   });
 
-  return data;
+  const normalizeItems = items.map((item) => {
+    return {
+      id: item.id,
+      item_name: item.item_name,
+      categories: item.categories.map((cat) => {
+        return cat.category.category_name;
+      })
+    };
+  });
+
+  const itemsCount = await prisma.items.groupBy({
+    by: ['id'],
+    _count: {
+      amount: true
+    }
+  });
+
+  const summary = [];
+
+  for (let i = 0; i < normalizeItems.length; i += 1) {
+    for (let j = 0; j < itemsCount.length; j += 1) {
+      if (normalizeItems[i].id !== itemsCount[j].id) continue;
+
+      const element = {
+        ...normalizeItems[i],
+        amount: itemsCount[j]._count.amount
+      };
+
+      summary.push(element);
+    }
+  }
+
+  return summary;
 });
